a08ca02ad595d9bf40207922380b4a7ad3942d28
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-10 16:55:12 +0530

    Update pair_sum.cpp

--- pair_sum.cpp
+++ pair_sum.cpp
@@ -1,77 +1,46 @@
-// check if any two numbers in the array add up to a target number
+// This program checks if any two numbers in the array add up to a target number
 
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-// vector<int> check_sum(vector<int> a,  int tar)  // brute force approach, O(n^2)
-// {
-//     int n= a.size();
-//     vector<int> ans;
-//     for(int i=0; i<n; i++)
-//     {
-//         for(int j=i+1; j<n; j++)
-//         {
-//             if(a[i]+a[j]== tar)
-//             {
-//                 ans.push_back(i);
-//                 ans.push_back(j);
-//             }
-//         }
-//     }
-//     return ans;
-// }
+// Function to find two indices whose values sum up to the target using an optimal approach
+vector<int> check_sum(vector<int> a, int tar) {
+    int n = a.size();         // Size of the input array
+    vector<int> ans;          // To store the result indices
+    
+    int st = 0, en = n - 1;   // Two pointers, one starting from the beginning and the other from the end
 
-vector<int> check_sum(vector<int> a,  int tar)   //optimal approach
-{
-    int n=a.size();
-    vector<int> ans;
-
-    int st=0, en=n-1;
-
-    while(st<en)
-    {
-        int sum=a[st]+a[en];
-        if(tar< sum)
-        {
-            en--;
-        }
-
-        else if(tar>sum)
-        {
-            st++;
-        }
-
-        else if(tar==sum)
-        {
-            ans.push_back(st);
+    while (st < en) {         // Loop until the two pointers meet
+        int sum = a[st] + a[en]; // Calculate the sum of the two elements at the pointers
+        if (tar < sum) {
+            en--;             // If the sum is greater than the target, move the end pointer left
+        } else if (tar > sum) {
+            st++;             // If the sum is less than the target, move the start pointer right
+        } else if (tar == sum) { // If the sum equals the target
+            ans.push_back(st); // Store the indices of the two elements
             ans.push_back(en);
-            return ans; 
+            return ans;       // Return the result
         }
     }
-    return ans;
+    return ans;               // Return an empty vector if no pair is found
 }
 
-int main()
-{
-    vector<int> arr={2,7,11,15};
-    int target;
+int main() {
+    vector<int> arr = {2, 7, 11, 15}; // Input array
+    int target;                       // Target sum
 
-    cout<<"enter target ";
-    cin>>target;
+    cout << "Enter target: ";
+    cin >> target;                    // Input the target sum
 
-    vector<int> ans = check_sum(arr, target);
+    vector<int> ans = check_sum(arr, target); // Call the function to find the pair
 
-    if (ans.empty())
-    {
+    if (ans.empty()) {                // If no pair is found
         cout << "No pair found";
-    }
-    else
-    {
-        cout << "Indices: " << ans[0] << ", " << ans[1]<<endl;
-        cout << "values: " << arr[ans[0]] << ", " << arr[ans[1]]<<endl;
+    } else {                          // If a pair is found
+        cout << "Indices: " << ans[0] << ", " << ans[1] << endl; // Print indices
+        cout << "Values: " << arr[ans[0]] << ", " << arr[ans[1]] << endl; // Print values
     }
 
-
-    return 0;
+    return 0; // End the program
 }
