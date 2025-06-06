ad8981a645afa4ac2863d40e0d14e966f206765e
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-06-06 13:28:49 +0530

    Update leet567 empty.cpp

--- leet567 empty.cpp
+++ leet567 empty.cpp
@@ -3,5 +3,5 @@ Given two strings s1 and s2, return true if s2 contains a permutation of s1, or 
 In other words, return true if one of s1's permutations is the substring of s2.
 Example 1:
 Input: s1 = "ab", s2 = "eidbaooo"
-Output: true
+Output: true 
 Explanation: s2 contains one permutation of s1 ("ba").  */
