b9183f2008a02a748f9e970df7d531bb8bd981f8
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-06-11 22:51:38 +0530

    Update leet121_stocks.cpp

--- leet121_stocks.cpp
+++ leet121_stocks.cpp
@@ -7,7 +7,7 @@ to buy one stock and choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction.
 If you cannot achieve any profit, return 0.  */
 
-#include<iostream>
+#include<iostream> 
 #include<vector>
 using namespace std;
 
