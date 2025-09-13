7cf38687f7d76d4e16c05d7c8fed7c15d22eedb3
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-09-13 15:05:43 +0530

    Add files via upload

--- rec_array.cpp
+++ rec_array.cpp
@@ -5,14 +5,30 @@ void print(int arr[],int n, int idx)
 {
     if(idx== n) return;
     cout<<arr[idx]<<" ";
-    print(arr, idx+1, n);
+    print(arr, n, idx+1);
 }
 
-void revPrint(int arr[], int n, int idx)
+void revPrint(int arr[], int n, int idx)  // to print array in reverse order without decrementing idx
 {
     if(idx== n) return;
 
     revPrint(arr, n, idx+1);
+    cout<<arr[idx]<<" ";
+}
+
+void revPrint2(int arr[], int idx)  // to print array in reverse order by decrementing idx without passing n
+{
+    if(idx<0) return;
+
+    cout<<arr[idx]<<" ";
+    revPrint2(arr, idx-1);
+}
+
+void printArray(int arr[], int idx)  // another way to print array in reverse order by decrementing idx without passing n
+{
+    if(idx<0) return;
+
+    printArray(arr, idx-1);
     cout<<arr[idx]<<" ";
 }
 
@@ -21,6 +37,16 @@ int main()
     int arr[5] = {1,2,3,4,5};
 
     print(arr, 5, 0);
+    cout<<endl;
+
     revPrint(arr, 5, 0);
+    cout<<endl;
+
+    revPrint2(arr, 4);
+    cout<<endl;
+
+    printArray(arr, 4);
+    cout<<endl;
+    
     return 0;
 }
