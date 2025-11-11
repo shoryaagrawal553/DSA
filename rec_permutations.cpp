14398ca5cb8f367dc5f3cb092e42046829a103ab
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-11-11 23:33:44 +0530

    rec_permutations.cpp

--- rec_permutations.cpp
+++ rec_permutations.cpp
@@ -1,3 +1,10 @@
+/* 46. Permutations
+* Given an numsay nums of distinct integers, return all the possible permutations. 
+* You can return the answer in any order.
+* Example 1:
+    Input: nums = [1,2,3]
+    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]] */
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -48,3 +55,22 @@ int main()
     }
     return 0;
 }
+
+
+// wihtout using visited array and temp 
+/*
+void recFunc(vector<int>& nums, vector<vector<int>>& ans, int idx)
+{
+    if(idx == nums.size())
+    {
+        ans.push_back(nums);
+        return;
+    }
+    for(int i=idx; i<nums.size(); i++)
+    {
+        swap(nums[i], nums[idx]);
+        recFunc(nums, ans, idx+1);
+        swap(nums[i], nums[idx]); // to reset the values for next index i.e tree for 2 at position 0
+    }
+} 
+*/
