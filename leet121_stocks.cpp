f530a013445d7741ed08e263cdba2dedd1058584
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-12-30 20:28:34 +0530

    leet121_stocks.cpp

--- leet121_stocks.cpp
+++ leet121_stocks.cpp
@@ -1,45 +1,46 @@
-/*You are given an array prices where prices[i] 
-is the price of a given stock on the ith day. 
+/* 121. Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+   */
 
-You want to maximize your profit by choosing a single day  
-to buy one stock and choosing a different day in the future to sell that stock.
-
-Return the maximum profit you can achieve from this transaction.
-If you cannot achieve any profit, return 0.  */
- 
-#include<iostream>  
+#include<iostream>
 #include<vector>
 using namespace std;
 
+int maxProfit(vector<int>& prices) 
+{
+    int n= prices.size();
+    int maxPrice=0, bestBuy=prices[0]; // Initialize bestBuy to the first price 
+    // maxPrice to 0 as we haven't made any transactions yet
+    // Iterate through the prices starting from the second day
 
-        int maxProfit(vector<int>& prices) 
-        {
-            int n= prices.size();
-            int maxPrice=0, bestBuy=prices[0]; // Initialize bestBuy to the first price 
-            // maxPrice to 0 as we haven't made any transactions yet
-            // Iterate through the prices starting from the second day
+    for(int i=1; i<n; i++)   //{3, 2, 6, 1, 4}
+    {
+        // If the current price is less than bestBuy, update bestBuy
+        // If the current price is greater than bestBuy, calculate the profit
+        // and update maxPrice if this profit is greater than the previous maxPrice
 
-            for(int i=1; i<n; i++)   //{3, 2, 6, 1, 4}
-            {
-                // If the current price is less than bestBuy, update bestBuy
-                // If the current price is greater than bestBuy, calculate the profit
-                // and update maxPrice if this profit is greater than the previous maxPrice
+        bestBuy= min(bestBuy, prices[i]); // Update bestBuy to the minimum price seen so far
 
-                bestBuy= min(bestBuy, prices[i]); // Update bestBuy to the minimum price seen so far
+        maxPrice= max(maxPrice, prices[i]-bestBuy); // check if selling today gives better profit
+                                                    // Calculate the profit if sold today(current i)
+    }
+    return maxPrice;
+}
 
-                maxPrice= max(maxPrice, prices[i]-bestBuy); // check if selling today gives better profit
-                                                            // Calculate the profit if sold today(current i)
-            }
-            return maxPrice;
-        }
+int main()
+{
+    // vector<int> prices = {7,1,5,3,6,4};    // Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5
+    // vector<int> prices = {7, 6, 4, 3, 1};  // No profit possible
+    vector<int> prices = {3, 2, 6, 1, 4};     // Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4
+    // vector<int> prices = {2, 4, 1};         // Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2
 
-        int main()
-        {
-            // vector<int> prices = {7,1,5,3,6,4};    // Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5
-            // vector<int> prices = {7, 6, 4, 3, 1};  // No profit possible
-            vector<int> prices = {3, 2, 6, 1, 4};     // Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4
-            // vector<int> prices = {2, 4, 1};        // Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2
-
-            cout << "Maximum profit: " << maxProfit(prices) << endl;
-            return 0;
-        }
+    cout << "Maximum profit: " << maxProfit(prices) << endl;
+    return 0;
+}
